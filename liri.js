51202ec40d40692fdd8b1636738eee4fd9fc4d02
Denise Middleton <demiddleton@bellsouth.net>
2019-09-20 19:20:01 -0400

    created code to display the conert venue and location

--- liri.js
+++ liri.js
@@ -26,12 +26,14 @@ var search = process.argv[3];
 
 function concertThis (search) {
     
-axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(
-  function (response) {
-    console.log(response);
-    // console.log("Name of the venue:", response.data?);
-    // console.log("Venue location:", response.data?); 
-    // console.log("Date of the Event:", response.data?);
+axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp")
+.then(function (response) {
+    //console.log(response);
+    console.log("**************************************************");
+    console.log("Name of the venue:", response.data[0].venue.name);
+    console.log("Venue location:", response.data[0].venue.city); 
+    console.log("Date of the Event:", response.data[0].venue.date);
+    console.log("**************************************************");
   })
   .catch(function (error) {
     console.log(error);
