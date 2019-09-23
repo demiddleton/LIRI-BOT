d2afb3646d4b1b8476c222243b140208f68de831
Denise Middleton <demiddleton@bellsouth.net>
2019-09-23 18:15:58 -0400

    Created code needed to run the 'movie-this' command

--- liri.js
+++ liri.js
@@ -19,7 +19,7 @@ var search = process.argv[3];
             spotifyThisSong(search);
             break;
         case "movie-this":
-            movieThis();
+            movieThis(search);
             break;
         case "do-what-it-says":
             doThis();
@@ -63,3 +63,46 @@ var spotify = new Spotify({
 //     console.log(error);
 //   });
   }
+
+  //Create function to handle the movie-thiscommand
+  
+
+function movieThis(search) {
+
+  axios.get("http://www.omdbapi.com/?t="+ search + "&y=&plot=short&apikey=trilogy")
+  .then(function(response) {
+    //console.log(response.data);
+   
+    console.log("*******************************************************");
+    console.log("Title: " + response.data.Title);
+    console.log("Year: " + response.data.Year);
+    console.log(" IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1]);
+    console.log("Country produced: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+    console.log("*******************************************************");
+
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+}
